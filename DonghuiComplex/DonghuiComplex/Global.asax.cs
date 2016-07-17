1a73420846c4d6e63187aca0adc453d672582067
zfhGithub <377108616@qq.com>
2016-07-17 19:59:33 +0800

    创建项目

---
+++ DonghuiComplex/DonghuiComplex/Global.asax.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.Security;
+using System.IO; 
+
+namespace Best
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // 在应用程序启动时运行的代码
+            // AuthConfig.RegisterOpenAuth();
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //using (StreamWriter sw = new StreamWriter("start.log"))
+            //{
+            //    sw.WriteLine("start.");
+            //}
+        }
+
+        void Application_End(object sender, EventArgs e)
+        {
+            //  在应用程序关闭时运行的代码 
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            //Exception ex = Server.GetLastError();
+            //using (StreamWriter sw = new StreamWriter ("error.log"))
+            //{
+            //    sw.WriteLine(ex.Message);
+            //}
+            // 在出现未处理的错误时运行的代码 
+        }
+    }
+}
